33418d02d8cfb9a21c7b9387d1aad83228bf6cb2
Bar11 <zxg259370@163.com>
2019-11-25 00:19:43 +0800

    ''

--- CMS/admin/models.py
+++ CMS/admin/models.py
@@ -1,8 +1,10 @@
 # encoding=UTF-8
-from .exts import db
-from datetime import datetime
+from exts import db
+
 
 class UserInfo(db.Model):
     __tablename__ = 'jq_userinfo'
     uid =db.Column(db.Integer, primary_key=True, autoincrement=True)
-    username 
+    username = db.Column(db.String(50), nullable=False)
+    password = db.Column(db.String(100),nullable=False)
+    email = db.Column(db.String(50), nullable=False,unique=True)
--- CMS/app.py
+++ CMS/app.py
@@ -3,12 +3,18 @@ from flask import Blueprint
 from admin.views import bp as admin_app
 from common.views import bp as common_app
 from front.views import bp as front_app
+from exts import db
 
-app = Flask(__name__)
-app.register_blueprint(admin_app)
-app.register_blueprint(common_app)
-app.register_blueprint(front_app)
-app.config.from_object('config')
+def create_app():
+    app=Flask(__name__)
+    app = Flask(__name__)
+    app.register_blueprint(admin_app)
+    app.register_blueprint(common_app)
+    app.register_blueprint(front_app)
+    app.config.from_object('config')
+    db.init_app(app)
+    return app
+
 
 @app.route('/')
 def hello_world():
@@ -16,4 +22,5 @@ def hello_world():
 
 
 if __name__ == '__main__':
+    app=create_app()
     app.run()
